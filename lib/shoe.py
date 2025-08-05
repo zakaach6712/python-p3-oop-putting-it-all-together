98d56d8e2869ed5d03a68dd5117dabb0fd6ca09f
zakaachzack@gmail.com <zakaachzack@gmail.com>
2025-08-05 22:43:32 +0300

    fixed the errrors in shoe.py

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand , size):
+        self.brand = brand
+        self.size = size
+        self.condition = None # this starts as undefined until   cobbled
+
+    @property 
+    def size(self):
+            return self._size
+
+    @size.setter
+    def size(self , value):
+            if isinstance(value, int):
+                self._size = value
+            else:
+                print("size must be an integer")
+
+    def cobble(self):
+         print("Your shoe is as good as new!")
+         self.condition = "New"                 
+      
     pass
